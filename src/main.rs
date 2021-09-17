6cdd839f884eafe60a999c9d57f3fdde5b3df962
robiot <robiot@users.noreply.github.com>
2021-09-17 22:55:07 +0300

    Bug fixes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
 use std::path::Path;
 use std::fs::metadata;
+use path_clean::PathClean;
 
 fn print_error<T: std::string::ToString>(err: T) {
     eprintln!("totall: {}", err.to_string());
 }
 
+fn print_help(args: Vec<String>) {
+    println!(
+        "Usage: {} <file/folder>", args[0]
+    )
+}
+
+fn absolute_path(path: impl AsRef<Path>) -> std::io::Result<std::path::PathBuf> {
+    let path = path.as_ref();
+    let absolute_path = if path.is_absolute() {
+        path.to_path_buf()
+    } else {
+        std::env::current_dir()?.join(path)
+    }.clean();
+
+    Ok(absolute_path)
+}
+
 fn get_all_files(dir_name: &str) -> Result<Vec<String>, std::io::Error>
 {
     let mut all_files: Vec<String> = vec!();
-    let paths = std::fs::read_dir(dir_name)?;
+    let paths;
+    if let Some(var) = std::fs::read_dir(dir_name).ok() {
+        paths = var;
+    } else {
+        return Ok(all_files);
+    }
 
     for path in paths {
-        let md = metadata(path.as_ref().unwrap().path())?;
+        let full_path = absolute_path(path.unwrap().path()).unwrap();
+        let full_path_str: &str = full_path.to_str().unwrap();
+        let md;
+        if let Some(var) = metadata(full_path.clone()).ok() {
+            md = var;
+        } else {
+            continue;
+        }
+
         if md.is_dir() {
-            all_files.append(&mut get_all_files(path.unwrap().path().to_str().unwrap())?);
+            all_files.append(&mut get_all_files(full_path_str)?);
         } else if md.is_file() {
-            all_files.append(&mut vec!(path.unwrap().path().to_str().unwrap().to_string()));
+            all_files.append(&mut vec!(full_path_str.to_string()));
         }
     }
     Ok(all_files)
@@ -30,18 +61,14 @@ fn main() {
 
     if args.len() < 2 {
         print_error("No path given.");
+        print_help(args);
         return;
     }
 
-    let mut path: &str = &args[1];
+    let path: &str = &args[1];
     if Path::new(path).exists() {
         let md = metadata(path).unwrap();
         if md.is_dir() {
-            let current_dir = std::env::current_dir().unwrap();
-            if path == "." {
-                path = current_dir.to_str().unwrap();
-            }
-
             let files: Vec<String> = match get_all_files(path) {
                 Ok(m) => m,
                 Err(err) => {
@@ -52,14 +79,27 @@ fn main() {
 
             let mut count: usize = 0;
             for file in files {
-                count += read_lines(file).unwrap();
+                count += match read_lines(file) {
+                    Ok(m) => m,
+                    Err(err) => {
+                        print_error(err);
+                        continue;
+                    }
+                };
             }
             println!("{}", count);
 
         } else if md.is_file() {
-            println!("{}", read_lines(path.to_string()).unwrap());
+            println!("{}", match read_lines(path.to_string()) {
+                Ok(m) => m,
+                Err(err) => {
+                    print_error(err);
+                    return;
+                }
+            });
         }
     } else {
-        print_error("Given path does not exist.")
+        print_error("Given path does not exist.");
+        print_help(args);
     }
 }
